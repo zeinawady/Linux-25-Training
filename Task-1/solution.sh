3472d2243812b0d7284a277ea9c21c543c4e250b
zeinawady <zeinawady@gmail.com>
2025-08-06 21:39:09 +0300

    Task submission.

---
+++ Task-1/solution.sh
@@ -0,0 +1,16 @@
+pwd
+cd gate-1
+rm trap
+cp key gate-2/
+cd gate-2/
+mv closed open
+mv open gate-3/
+cd gate-3
+rm trap
+cd gate-4/
+rm -r dimension-x
+rm -r dimension-y
+cd earth
+mkdir home
+cd home
+touch bed
