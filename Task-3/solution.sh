839731e5f275f56db23518df3b6648e9e8d24aea
Zeina Wady <126752492+zeinawady@users.noreply.github.com>
2025-08-15 18:11:01 +0300

    Update solution.sh

--- Task-3/solution.sh
+++ Task-3/solution.sh
@@ -1,1 +1,16 @@
+sudo useradd hero
+sudo useradd intruder
+sudo userdel intruder
+sudo groupadd warriors
+sudo groupadd healers
+sudo usermod -aG warriors hero
+sudo useradd sage
+sudo usermod -aG healers sage
+nano treasure.txt
+sudo chmod 000 treasure.txt
+sudo chown hero treasure.txt
+sudo chmod 600 treasure.txt
+sleep 100 &
+ps
+killall sleep
 
